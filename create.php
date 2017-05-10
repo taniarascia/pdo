d3710dc22506354d7c098a0b4aef3c67f6294d59
Tania Rascia <taniarascia@gmail.com>
2017-05-10 17:34:08 -0500

    Add Read

--- create.php
+++ create.php
@@ -17,7 +17,7 @@ if ($_POST)
 		$connection = new PDO($dsn, $username, $password, $options);
 
 		$sql = "INSERT INTO users (firstname, lastname, email, age, location)
-						VALUES (:firstname, :lastname, :email, :age, :location)";
+            VALUES (:firstname, :lastname, :email, :age, :location)";
 
 		$statement = $connection->prepare($sql);
 
@@ -45,8 +45,9 @@ if ($_POST)
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="css/primitive.css">
 	<title>Create</title>
+	
+	<link rel="stylesheet" href="css/primitive.css">
 
 </head>
 
@@ -55,19 +56,19 @@ if ($_POST)
 	<div class="small-container">
 		<h1>Add a user</h1>
 
-			<form method="post">
-				<label for="firstname">First Name</label>
-				<input type="text" name="firstname" id="firstname">
-				<label for="lastname">Last Name</label>
-				<input type="text" name="lastname" id="lastname">
-				<label for="email">Email Address</label>
-				<input type="text" name="email" id="email">
-				<label for="age">Age</label>
-				<input type="text" name="age" id="age">
-				<label for="location">Location</label>
-				<input type="text" name="location" id="location">
-				<input type="submit" value="Submit">
-			</form>
+		<form method="post">
+			<label for="firstname">First Name</label>
+			<input type="text" name="firstname" id="firstname">
+			<label for="lastname">Last Name</label>
+			<input type="text" name="lastname" id="lastname">
+			<label for="email">Email Address</label>
+			<input type="text" name="email" id="email">
+			<label for="age">Age</label>
+			<input type="text" name="age" id="age">
+			<label for="location">Location</label>
+			<input type="text" name="location" id="location">
+			<input type="submit" value="Submit">
+		</form>
 	</div>
 
 </body>
--- read.php
+++ read.php
@@ -1,113 +1,35 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 /**
  * Function to query information and display table
  *
  */
 
-//if ($_POST) 
-//{
+if ($_POST) 
+{
 
-	require_once "config.php";
+	require_once realpath(__DIR__) . "/config.php";
 	require_once "common.php";
 
-	try 
-	{
-		$connection = new PDO($dsn, $username, $password, $options);
+	$connection = new PDO($dsn, $username, $password, $options);
 
-		/*$sql = "SELECT *
-						FROM users
-						WHERE location = :location";
-		
-		$statement = $connection->prepare($sql);
-		
-		$statement->execute(array(
-			":location" => "Portland")
-		);
+	$sql= "SELECT * 
+         FROM users
+         WHERE location = :location";
+	
+	$location = $_POST['location'];
 
-		$result = $connection->query($sql);*/
-		
-	 $statement = $connection->prepare(
-        'SELECT
-           *
-        FROM
-            users
-        WHERE
-            location = :location'
-    );
-		
-	$result = $statement->execute(
-        array('location' => 'Portland', )
-    );
-		
-	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	$statement = $connection->prepare($sql);
+	$statement->bindParam(':location', $location, PDO::PARAM_STR);
+	$statement->execute();
 
-    return $row;
+	$result = $statement->fetchAll();
 
-		if ($result->num_rows > 0) { ?>
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email Address</th>
-							<th>Age</th>
-							<th>Location</th>
-							<th>Date</th>
-						</tr>
-					</thead>
-					<tbody> 
-
-			<?php
-			while ($row = $statement->fetch()) {
-				// place all rows into an array
-				$rows[] = $row;
-			}
-			// echo out each row as a table item
-			foreach ($rows as $row) { ?>
-				<tr>
-					<td><?php echo $row["id"]; ?></td>
-					<td><?php echo $row["firstname"]; ?></td>
-					<td><?php echo $row["lastname"]; ?></td>
-					<td><?php echo $row["email"]; ?></td>
-					<td><?php echo $row["age"]; ?></td>
-					<td><?php echo $row["location"]; ?></td>
-					<td><?php echo $row["date"]; ?> </td></tr>
-
-			<?php	
-			} ?>
-
-				</table>
-
-				<form method="post" action="export-csv.php">
-					<input type="hidden" name="location" value="<?php echo $location; ?>">
-					<input class="btn btn-primary" type="submit" value="Download CSV File">
-				</form>
-
-			<?php
-			} else { 
-					echo "No entries found.";
-			}
-	}
-
-	catch(PDOException $e) 
-	{
-		echo $sql . "<br>" . $e->getMessage();
+	foreach($result as $row){
+			echo "<li>{$row['location']}</li>";
 	}
 	
-//}
-
-/*
-if (isset($_POST['location'])) { 
-	printRows($_POST['location']);
-} else {
-	echo "Error: No location selected. Please go back and enter a date.";
-} */
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -117,8 +39,9 @@ if (isset($_POST['location'])) {
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="css/primitive.css">
 	<title>Read</title>
+	
+	<link rel="stylesheet" href="css/primitive.css">
 
 </head>
 
@@ -126,16 +49,12 @@ if (isset($_POST['location'])) {
 
 	<div class="small-container">
 		<h1>Read</h1>
-
-		<div class="row">
-			<div class="offset-md-3 col-md-6">
-				<form method="post">
-					<label for="location">Location</label>
-					<input type="text" id="location" name="location">
-					<input type="submit" value="View Results">
-				</form>
-			</div>
-		</div>
+		
+		<form method="post">
+			<label for="location">Location</label>
+			<input type="text" id="location" name="location">
+			<input type="submit" value="View Results">
+		</form>
 	</div>
 
 </body>
