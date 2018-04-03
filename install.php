9fd1c3949ec2d95ac9cd4e319dc8a525affc0566
Tania Rascia <taniarascia@gmail.com>
2018-04-03 16:24:53 -0500

    Update install.php

--- install.php
+++ install.php
@@ -8,16 +8,12 @@
 
 require "config.php";
 
-try 
-{
-	$connection = new PDO("mysql:host=$host", $username, $password, $options);
-	$sql = file_get_contents("data/init.sql");
-	$connection->exec($sql);
-	
-	echo "Database and table users created successfully.";
-}
-
-catch(PDOException $error)
-{
-	echo $sql . "<br>" . $error->getMessage();
+try {
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    $sql = file_get_contents("data/init.sql");
+    $connection->exec($sql);
+    
+    echo "Database and table users created successfully.";
+} catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
 }
