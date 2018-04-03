8f5365f6fe332343d01ce3de15de8495a1092994
Tania Rascia <taniarascia@gmail.com>
2018-04-03 16:23:29 -0500

    Update create.php

--- public/create.php
+++ public/create.php
@@ -7,66 +7,57 @@
  */
 
 
-if (isset($_POST['submit']))
-{
-	
-	require "../config.php";
-	require "../common.php";
+if (isset($_POST['submit'])) {
+    require "../config.php";
+    require "../common.php";
 
-	try 
-	{
-		$connection = new PDO($dsn, $username, $password, $options);
-		
-		$new_user = array(
-			"firstname" => $_POST['firstname'],
-			"lastname"  => $_POST['lastname'],
-			"email"     => $_POST['email'],
-			"age"       => $_POST['age'],
-			"location"  => $_POST['location']
-		);
+    try  {
+        $connection = new PDO($dsn, $username, $password, $options);
+        
+        $new_user = array(
+            "firstname" => $_POST['firstname'],
+            "lastname"  => $_POST['lastname'],
+            "email"     => $_POST['email'],
+            "age"       => $_POST['age'],
+            "location"  => $_POST['location']
+        );
 
-		$sql = sprintf(
-				"INSERT INTO %s (%s) values (%s)",
-				"users",
-				implode(", ", array_keys($new_user)),
-				":" . implode(", :", array_keys($new_user))
-		);
-		
-		$statement = $connection->prepare($sql);
-		$statement->execute($new_user);
-	}
-
-	catch(PDOException $error) 
-	{
-		echo $sql . "<br>" . $error->getMessage();
-	}
-	
+        $sql = sprintf(
+                "INSERT INTO %s (%s) values (%s)",
+                "users",
+                implode(", ", array_keys($new_user)),
+                ":" . implode(", :", array_keys($new_user))
+        );
+        
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+    
 }
 ?>
 
 <?php require "templates/header.php"; ?>
 
-<?php 
-if (isset($_POST['submit']) && $statement) 
-{ ?>
-	<blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
-<?php 
-} ?>
+<?php if (isset($_POST['submit']) && $statement) { ?>
+    <blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+<?php } ?>
 
 <h2>Add a user</h2>
 
 <form method="post">
-	<label for="firstname">First Name</label>
-	<input type="text" name="firstname" id="firstname">
-	<label for="lastname">Last Name</label>
-	<input type="text" name="lastname" id="lastname">
-	<label for="email">Email Address</label>
-	<input type="text" name="email" id="email">
-	<label for="age">Age</label>
-	<input type="text" name="age" id="age">
-	<label for="location">Location</label>
-	<input type="text" name="location" id="location">
-	<input type="submit" name="submit" value="Submit">
+    <label for="firstname">First Name</label>
+    <input type="text" name="firstname" id="firstname">
+    <label for="lastname">Last Name</label>
+    <input type="text" name="lastname" id="lastname">
+    <label for="email">Email Address</label>
+    <input type="text" name="email" id="email">
+    <label for="age">Age</label>
+    <input type="text" name="age" id="age">
+    <label for="location">Location</label>
+    <input type="text" name="location" id="location">
+    <input type="submit" name="submit" value="Submit">
 </form>
 
 <a href="index.php">Back to home</a>
